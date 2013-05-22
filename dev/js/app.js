7050e8c02f57364dafbbf96f884215fa8eb8fe95
Kevin <kevin@theranking.com>
2013-05-22 16:29:00 +0200

    Added minify options in the website
    * added xem-minifier
    * now it's possible to minify the result after getting grouped mqs
    * updated the app to the changes in mqhelper module

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,46 +1,62 @@
+/* Author: Kevin Purnelle
+
+	MqHelper website app
+*/
+
 
 var App = (function ($) {
 
 	"use strict";
 
 	var app = {};
-	var minifierURL = "http://cssminifier.com/raw";
 
 	app.options = {
 		stripped : null,
 		groups : null,
 		normal : true,
 		position : "bottom",
-		minify: false
+		minify : {
+			basic: false,
+			zeros: false,
+			rgbcolors: false,
+			oldIeCompatibility: false
+		}
 	}
 
-
-
+	app.results = {}
 
 	/* Add some css code example in the source textarea
 	--------------------------------------------------------------- */
 	function setSourceExample() {
 
-		var exampleSource = " /*Example*/\n	.somerule {\n	property1:value;\n}\n@media screen and (    max-width:979px){\n	.rule979-one {\n		property1:value;\n		property2:value\n	}\n	.rule979-two {\n		property1:value;\n		property2:value\n	}\n}\n@media screen and (max-width:480px){\n	.rule480-one {\n		property1:value;\n		property2:value\n	}\n	.rule480-two {\n		property1:value;\n		property2:value\n	}\n}\n@media screen AND (max-width:979px){\n	.rule979-again {\n		property1:value;\n	}\n}\n@media screen and (max-width:480px){\n	.rule480-again {\n		property1:value;\n		property2:value\n	}\n	.rule480-again2 {\n		property1:value;\n		property2:value\n	}\n}\n@media screen and (max-width:979px){\n	.rule979-again2 {\n		property1:value;\n	}\n}";
+		var exampleSource = " /*Example*/\n.somerule {\n    color: rgb(100,250,90);\n}\n@media screen and (    max-width:979px){\n	.rule979-one {\n		background-color: #ccddee;\n		color: rgb(255,255,255)\n	}\n	.rule979-two {\n		opacity:0.5;\n		margin: 15px 0px 15px 10px\n	}\n}\n@media screen and (max-width:480px){\n	.rule480-one {\n		property1:value;\n		property2:value\n	}\n	.rule480-two {\n		property1:value;\n		property2:value\n	}\n}\n@media screen AND (max-width:979px){\n	.rule979-again {\n		property1:value;\n	}\n}\n@media screen and (max-width:480px){\n	.rule480-again {\n		property1:value;\n		property2:value\n	}\n	.rule480-again2 {\n		property1:value;\n		property2:value\n	}\n}\n@media screen and (max-width:979px){\n	.rule979-again2 {\n		property1:value;\n	}\n}";
 		app.$.source.val(exampleSource)
 	}
 
 
-	/* Get the needed objects, bind events to UI
+	/* Get the needed DOM objects, bind events to UI, set example
 	--------------------------------------------------------------- */
 	app.init = function () {
 
+		// dom
 		app.$ = {
-			form : $('#test-form'),
+			optionsWrapper : $('#processOptions'),
+			form : $('#mqh-form'),
 			groups : $('#groups'),
 			normal : $('#normal'),
-			position : $('input:radio[name=mqposition]'),
 			resultsWrapper : $('#results-container'),
 			source : $('#textarea-source-code'),
 			stripped : $('#stripped'),
-			minify: $('#minify')
+			invisiblesAndComments: $('#invisiblesAndComments'),
+			zeros: $('#zeros'),
+			rgbcolors: $('#rgbcolors'),
+			oldIeCompatibility: $('#oldIeCompatibility'),
+			btnToggleOptions: $('#btn-toggle-options')
 		}
 
+		app.$.optionsWrapper.hide()
+
+		// events
 		app.$.normal.on('click', function (event) {
 			app.options.normal = $(this).is(':checked')
 		})
@@ -53,24 +69,80 @@ var App = (function ($) {
 			app.options.groups = $(this).is(':checked')
 		})
 
-		app.$.position.on('click', function (event) {
-			app.options.position = $(this).val()
+		app.$.invisiblesAndComments.on('click', function (event) {
+			app.options.minify.basic = $(this).is(':checked')
 		})
 
-		app.$.minify.on('click', function (event) {
-			app.options.minify = $(this).is(':checked')
+		app.$.zeros.on('click', function (event) {
+			app.options.minify.zeros = $(this).is(':checked')
 		})
 
-		app.$.form.on('submit', function (event) {
-
-			var result;
-
-			event.preventDefault()
-			result = MqHelper.process(app.$.source.val())
-			app.$.resultsWrapper.html(app.resultToHtml(result))
+		app.$.rgbcolors.on('click', function (event) {
+			app.options.minify.rgbcolors = $(this).is(':checked')
 		})
 
+		app.$.oldIeCompatibility.on('click', function (event) {
+			app.options.minify.oldIeCompatibility = $(this).is(':checked')
+		})
+
+		app.$.btnToggleOptions.on('click', function (event) {
+			app.$.optionsWrapper.toggle()
+		})
+
+		app.$.form.on('submit', app.processCss)
+
+		// set example
 		setSourceExample()
+	}
+
+
+	/* Process css when the form submit button is clicked
+	--------------------------------------------------------------- */
+	app.processCss = function processCss(event) {
+		event.preventDefault()
+		app.results = MqHelper.process(app.$.source.val())
+		if (app.options.minify.basic) {
+			app.results.fullCss = XemMinifier.process(app.results.fullCss, app.options.minify)
+		}
+		app.$.resultsWrapper.html(app.resultToHtml(app.results))
+		app.getAndDisplayStats()
+
+		setTimeout(function () {
+			var $statsWrapper = $('.stats-wrapper');
+			$statsWrapper.fadeOut(250, function () {
+				$statsWrapper.remove()
+			})
+		}, 10000)
+	}
+
+
+	/* Get and Display stats
+	--------------------------------------------------------------- */
+	app.getAndDisplayStats = function () {
+
+		var html = "";
+		var sourceSize = app.$.source.val().length;
+		var resultSize = app.results.fullCss.length;
+
+		html += '<div class="stats-wrapper">'
+		html += '<h3 class="title">Results stats</h3>'
+		html += '<ul>'
+		html += '<li>'
+		html += "Source size: " + sourceSize + " bytes"
+		html += '</li>'
+		html += '<li>'
+		html += "Result size: " + resultSize + " bytes"
+		html += '</li>'
+		html += '<li>'
+		html += "Media queries found in source: " + app.results.stats.mqFound
+		html += '</li>'
+		html += '<li>'
+		html += "Media queries after defrag: " + app.results.stats.mqDefrag
+		html += '</li>'
+		html += '</ul>'
+		html += '</div>'
+
+		$('body').append(html)
 	}
 
 	/* Build some markup that contains the results ready for display
@@ -80,37 +152,13 @@ var App = (function ($) {
 		var html = "";
 		var ordered = "";
 		var code = "";
+		var minifyOptions = {};
 
 		if (app.options.normal) {
 
 			html += "<h2> Your code with grouped media queries </h2>";
 
-			if (result.mqs !== null) {
-
-				for (var i = 0, len = result.mqs.length ; i < len ; i++) {
-
-					ordered += result.mqs[i].query + " {\n";
-					ordered += result.mqs[i].rules + "\n}\n";
-				}
-			}
-
-			(app.options.position === "bottom") ?
-			code += result.stripped + "\n" + ordered :
-			code += ordered + "\n" + result.stripped
-
-			if (app.options.minify) {
-
-				$.ajax(minifierURL, {
-					type: "POST",
-					data: {
-						input: code
-					},
-					success: function (data, textStatus, jqXHR) {
-						code = data
-					},
-					dataType: "text"
-				});
-			}
+			code = app.results.fullCss
 
 			html += "<div>";
 			html += "<textarea>";
@@ -129,7 +177,7 @@ var App = (function ($) {
 				html += "<h3> Code stripped from media queries </h3>";
 				html += "<div>";
 				html += "<textarea>";
-				html += result.stripped;
+				html += app.results.stripped;
 				html += "</textarea>";
 				html += "</div>";
 			}
@@ -139,12 +187,12 @@ var App = (function ($) {
 
 				html += "<div>"
 
-				for (var i = 0, len = result.mqs.length ; i < len ; i++) {
+				for (var i = 0, len = app.results.mqs.length ; i < len ; i++) {
 
-					html += "<h3>" + result.mqs[i].query + "</h3>";
+					html += "<h3>" + app.results.mqs[i].query + "</h3>";
 					html += "<textarea>";
-					html += result.mqs[i].query + " {\n";
-					html += result.mqs[i].rules + "\n}";
+					html += app.results.mqs[i].query + " {\n";
+					html += app.results.mqs[i].rules + "\n}";
 					html += "</textarea>";
 				}
 
